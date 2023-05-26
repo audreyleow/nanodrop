4efc86ff016e99b473510c972881aa2d9fefa12e
nelsontky <nelson@ccb.wtf>
2023-05-27 04:20:31 +0800

    fix: allow cors in files

--- apps/nanodrop-files/src/index.ts
+++ apps/nanodrop-files/src/index.ts
@@ -1,15 +1,29 @@
+import { createCors } from "itty-cors";
 import { Router } from "itty-router";
 
 import { handleGetFile } from "./services/get-file.service";
 import { handleFileUpload } from "./services/upload-file.service";
+import { Env } from "./types";
+
+const { preflight, corsify } = createCors();
 
 const router = Router();
 
-router.get("/*", handleGetFile);
+router.all("*", preflight);
+
+router.get("/*", async (request: Request, env: Env) =>
+  corsify(await handleGetFile(request, env))
+);
 
 router.post("/upload", handleFileUpload);
 
 router.all("*", () => new Response("404, not found!", { status: 404 }));
 
-const fetchHandler = { fetch: router.handle };
+const fetchHandler = {
+  fetch: (request: Request, ...extra: any) =>
+    router
+      .handle(request, ...extra)
+      .catch(() => new Response("Error", { status: 500 })),
+};
+
 export default fetchHandler;
