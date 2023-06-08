ea8fb2a7a350a747977fd82d324a39f37f4b7c0a
nelsontky <nelson@ccb.wtf>
2023-06-08 14:12:30 +0800

    feat: use react router in nextjs

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,20 @@
-module.exports = {
+const config = {
   reactStrictMode: true,
   transpilePackages: ["@nanodrop/sdk"],
+  images: {
+    unoptimized: true,
+  },
 };
+
+if (process.env.NODE_ENV === "development") {
+  config.rewrites = async () => [
+    {
+      source: "/:any*",
+      destination: "/",
+    },
+  ];
+} else {
+  config.output = "export";
+}
+
+module.exports = config;
--- apps/web/src/features/launch/page.tsx
+++ apps/web/src/features/launch/page.tsx
@@ -1,16 +1,17 @@
 "use client";
 
-import MuiStepper from "@/common/components/mui/Stepper";
+// import MuiStepper from "@/common/components/mui/Stepper";
 
-import DetailsForm from "../../common/components/forms/DetailsForm";
+// import DetailsForm from "../../common/components/forms/DetailsForm";
 import styles from "./styles.module.css";
 
 export default function Page() {
-  return (
-    <div>
-      <div className={styles.heading}>Launch Collection</div>
-      <MuiStepper />
-      <DetailsForm />
-    </div>
-  );
+  return null;
+  // return (
+  //   <div>
+  //     <div className={styles.heading}>Launch Collection</div>
+  //     <MuiStepper />
+  //     <DetailsForm />
+  //   </div>
+  // );
 }
