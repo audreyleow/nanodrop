2706f65f33e8d5827d8f920e80dce2649507e91e
nelsontky <nelson@ccb.wtf>
2023-06-25 09:32:18 +0800

    refactor: use env for mongodb

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,15 +4,15 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { UsersModule } from "./users/users.module";
 import { NanoMachinesModule } from "./nano-machines/nano-machines.module";
 import { SolanaService } from "./solana/solana.service";
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
   imports: [
-    MongooseModule.forRoot("mongodb://localhost:27017/nanodrop", {
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URL, {
       autoIndex: true,
       autoCreate: true,
     }),
-    ConfigModule.forRoot(),
     UsersModule,
     NanoMachinesModule,
     AuthModule,
